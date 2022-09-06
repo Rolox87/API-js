e0a89d4e55459b4e5722d8c6c5de35869a00051f
Rolando Hinojosa <103226781+Rolox87@users.noreply.github.com>
2022-09-05 21:00:59 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+import { prepareConfigChart } from "./utilities/helpers.js";
+import { validate } from "./utilities/helpers.js";
+
+
+const API = "https://mindicador.cl/api/";
+
+const selectBox = document.getElementById("select-box"),
+  inputBox = document.getElementById("input-box"),
+  submitBtn = document.getElementById("submit-btn"),
+  resultText = document.getElementById("result");
+
+const getValues = async (indicator = "") => {
+  try {
+    const options = { method: "GET" },
+      res = await fetch(API.concat(indicator), options);
+    console.log(res);
+    return res.ok ? await res.json() : alert("Recursos no disponible");
+  } catch (err) {
+    return err.message;
+  }
+};
+
+const fillSelectBox = async () => {
+  const values = await getValues();
+  if (values) {
+    let html = "";
+    Array.from(Object.entries(values)).forEach((value) => {
+      if (
+        value[1].codigo === undefined ||
+        value[1].codigo === "tpm" ||
+        value[1].codigo === "tasa_desempleo"
+      ) {
+        console.log(`indicador ${value[1].codigo} ignorado`);
+      } else {
+        html += `<option value="${value[1].codigo}">${value[1].nombre}</option>`;
+      }
+    });
+    selectBox.innerHTML = html;
+  }
+};
+const renderChart = async (indicator) => {
+  const data = await getValues(indicator),
+    chartStatus = Chart.getChart("chart");
+  chartStatus != undefined ? chartStatus.destroy() : false;
+  Chart.defaults.color = "#d6ff00";
+  new Chart("chart", prepareConfigChart("chart", data));
+};
+
+const getResults = async (_from, _to) => {
+  const values = await getValues(),
+    formatResult = (_from / values[`${_to}`].valor).toFixed(2);
+  let symbol = "$";
+  if (_to === "euro") {
+    symbol = "€";
+  } else if (_to === "bitcoin") {
+    symbol = "฿";
+  }
+  resultText.innerHTML = `<span>${symbol} ${formatResult.replace(
+    ".",
+    ","
+  )}</span>`;
+};
+
+window.addEventListener("load", () => {
+  document.body.className = "loaded";
+  
+  fillSelectBox();
+  submitBtn.addEventListener("click", () => {
+
+
+    validate(Number(inputBox.value))
+      ? getResults(Number(inputBox.value), selectBox.value) &&
+      renderChart(selectBox.value)
+      : "";
+  });
+  document.addEventListener("keydown", (e) => {
+    e.key === "Enter"
+      ? inputBox.value != ""
+        ? getResults(Number(inputBox.value), selectBox.value) &&
+        renderChart(selectBox.value)
+        : alert("Debe ingresar un monto para buscar")
+      : "";
+  });
+});
---
+++ assets/js/utilities/helpers.js
@@ -0,0 +1,112 @@
+const getDataColors = (opacity) => {
+  const colors = [
+    "#7448c2",
+    "#21c0d7",
+    "#d99e2b",
+    "#cd3a81",
+    "#9c99cc",
+    "#e14eca",
+    "#ffffff",
+    "#ff0000",
+    "#d6ff00",
+  ];
+
+  return colors.map((color) => (opacity ? `${color + opacity}` : color));
+};
+
+export const prepareConfigChart = (chartId, data) => {
+  document.getElementById(chartId).parentNode.style.opacity = "1";
+  const ctx = document.getElementById(chartId).getContext("2d");
+  // gradient
+  let gradient = ctx.createLinearGradient(0, 0, 0, 400);
+  gradient.addColorStop(0, "rgba(250, 220, 0, 1)");
+  gradient.addColorStop(1, "rgba(250, 130, 0, 0.3)");
+  // delayed -> animation
+  let delayed;
+
+  const firstTenEntries = data.serie.filter((value, index) => index < 10),
+    xLabels = firstTenEntries.map((ele) => ele.fecha.substring(0, 10)),
+    yLabels = firstTenEntries.map((ele) => ele.valor);
+  const title = "Historial 10 últimos cambios";
+
+  const config = {
+    type: "line",
+    data: {
+      labels: xLabels,
+      datasets: [
+        {
+          label: title,
+          data: yLabels,
+          backgroundColor: gradient,
+          borderColor: getDataColors()[8],
+          pointBorderColor: getDataColors()[6],
+          fill: true,
+          borderWidth: 2,
+          pointBorderWidth: 5,
+          tension: 0.2,
+
+        },
+      ],
+    },
+    options: {
+      radius: 3,
+      hoverRadius: 12,
+      responsive: true,
+      animation: {
+        duration: 2000,
+        onComplete: () => {
+          delayed = true;
+        },
+        delay: (context) => {
+          let delay = 0;
+          if (context.type === "data" && context.mode === "default" && !delayed) {
+            delay = context.dataIndex * 300 + context.datasetIndex * 100;
+          }
+          return delay;
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: getDataColors()[8],
+          },
+        },
+        y: {
+          ticks: {
+            color: getDataColors()[2],
+            callback: function (value) {
+              value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+              return "$" + value + " CLP"
+            }
+          },
+        },
+      },
+    },
+  };
+  return config;
+};
+
+const getMessageAlert = (text, icon) => {
+  return Swal.fire({
+    stopKeydownPropagation: true,
+    icon: icon,
+    title: "Oops...",
+    text: text,
+    background: "#393939",
+    color: "#ccc",
+  });
+};
+
+export const validate = (value) => {
+  if (value === 0) {
+    getMessageAlert("Debe ingresar valores decimales mayores a 0", "info");
+  } else if (value === "") {
+    getMessageAlert("Debe ingresar un valor para buscar", "info");
+  } else if (value < 0) {
+    getMessageAlert(
+      "Debe ingresar solo valores decimales positivos mayores a 0",
+      "info"
+    );
+  }
+  return value > 0 && value != "";
+};
